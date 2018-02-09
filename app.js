479b8426ab414eec4914c1ac6ccc7398ca79a35d
Unknown <biotech@gmail.com>
2018-02-08 21:08:27 -0800

    feat: channels, groups, and more!
    - Bo tells you when connected and if he needs a channel invite
    - Grabs a list of channels, private channels, and users to lookup if needed
    - Some code cleanup

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const Flickr = require('flickr-sdk');
 // Slack setup
 var fullTeamList = [];
 var fullChannelList = [];
+var needInvitations = [];
 
 // Setup temp download directory
 var tmpDir = './tmp'
@@ -28,17 +29,43 @@ var flickrAuth = Flickr.OAuth.createPlugin(
 );
 
 const controller = Botkit.slackbot({
-    debug: true,
-    json_file_store: 'history',
-    // studio_token: process.env.BOTKIT_STUDIO_TOKEN
+    debug: true
 });
 
 const bot = controller.spawn({
     token: process.env.SLACK_API_TOKEN,
     retry: 30
 }).startRTM(function(err, bot, payload) {
-  // adminNotify(bot, `Hello <!channel> - I just connected!`)
-  // adminNotify(bot, `Hello <!channel> - I just connected!`)
+  adminNotify(bot, `Hello - I just connected!`)
+  if (err) {throw new Error(err);}
+
+  // Update the channels and users list on startup
+
+  // @ https://api.slack.com/methods/users.list
+  bot.api.users.list({}, function (err, response) {
+    if (response.hasOwnProperty('members') && response.ok) {
+      fullTeamList = response.members.map(x => ({name: x.name, id: x.id}))
+    }
+  });
+
+  // @ https://api.slack.com/methods/channels.list
+  bot.api.channels.list({}, function (err, response) {
+    if (response.hasOwnProperty('channels') && response.ok) {
+      fullChannelList = [...fullChannelList, ...response.channels.map(x => ({name: x.name, id: x.id}))]
+      needInvitations = response.channels.filter(x => (!x.is_member)).map(x => `<#${x.id}>`)
+      if (needInvitations.length != 0) {
+        adminNotify(bot, `Hey! I need invitations to these channels in monitor them: ${needInvitations}`)  
+      }
+    }
+  });
+
+  // @ https://api.slack.com/methods/groups.list
+  bot.api.groups.list({}, function (err, response) {
+    if (response.hasOwnProperty('groups') && response.ok) {
+      fullChannelList = [...fullChannelList, ...response.groups.map(x => ({name: x.name, id: x.id}))]
+    }
+  });
+
 });
 
 // Replies
@@ -52,17 +79,12 @@ const replies = [
 //     console.log(message);
 // });
 
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
-    bot.reply(message,getRandomItem(replies));
-});
-
 controller.hears('.*', 'direct_message,direct_mention,mention', function(bot, message) {
     bot.reply(message,getRandomItem(replies));
 });
 
 controller.on('file_share', (bot, msg) => {
-  console.log(msg);
-  var tmpFile = `${tmpDir}/${msg.file.name}-${getId()}`
+  var tmpFile = `${tmpDir}/${getId()}-${msg.file.name}`
   var file = fs.createWriteStream(tmpFile)
   var authToken = {'bearer': process.env.SLACK_API_TOKEN}
   var req = request.get(msg.file.url_private_download, {'auth': authToken}).pipe(file)
@@ -71,41 +93,36 @@ controller.on('file_share', (bot, msg) => {
     // Upload the file to flickr
     var flickr = new Flickr(flickrAuth);
     var defaultTags = [`Uploaded by ${msg.user_profile.display_name}`, 'BnH Slack']
+    var channelNames = msg.file.channels.map(chanId => '#'+getNameFromId(chanId, fullChannelList))
+    var groupNames = msg.file.groups.map(chanId => '#'+getNameFromId(chanId, fullChannelList))
     var upload = new Flickr.Upload(flickrAuth, tmpFile, {
       title: msg.file.title,
       description: (typeof msg.file.initial_comment === 'undefined') ? '' : msg.file.initial_comment.comment,
-      tags: [...msg.file.channels, ...msg.file.groups, ...defaultTags].map(x => `"${x}"`).join(' ')
-      // tags: '"some space" "other space"'
+      tags: [...channelNames, ...groupNames, ...defaultTags].map(x => `"${x}"`).join(' ')
     });
 
     upload.then(function (res) {
-      console.log('yay!', res.body);
+      console.log('Successfully uploaded to Flickr');
       // Image uploaded successfully, now lets remove the file on disk
       fs.unlink(tmpFile, function(err) {
-        if (err) throw err;
+        if (err) console.log(err);
         console.log(`${tmpFile} removed`)
       })
       // add to album
       var ps = flickr.photosets.addPhoto({photoset_id: process.env.FLICKR_DEFAULT_ALBUM, photo_id: res.body.photoid._content})
       ps.then(function (res) {
-        console.log('added!', res.body);
+        console.log('Added file to album', res.body);
       }).catch(function(err) {
-        console.log('bonk', err);
+        console.log('Something went wrong!', err);
       })
     }).catch(function (err) {
-      console.error('bonk', err);
+      console.error('Something went wrong', err);
     }); 
   })
-  
-
 })
-// controller.on('direct_message,direct_mention,mention', function(bot, message) {
-//     controller.studio.runTrigger(bot, message.text, message.user, message.channel).catch(function(err) {
-//         bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
-//     });
-// });
 
-////////
+
+//////// Helper functions
 // get random item from a Set
 function getRandomItem(set) {
     let items = Array.from(set);
@@ -122,4 +139,9 @@ function adminNotify(bot, msg) {
     text: msg,
     channel: process.env.SLACK_ADMIN_CHANNEL
   })
+}
+
+function getNameFromId(id, l) {
+  x = l.find(x => x.id == id)
+  return (typeof(x) == 'undefined') ? id : x.name
 }
